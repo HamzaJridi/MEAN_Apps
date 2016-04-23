70304b95c6f75425d7770cc0c4807038741e8e7a
Hamza Jridi <jrid_hamza@yahoo.fr>
2016-04-23 15:01:52 +0100

    use the http server and test it by sending a http request

--- server.js
+++ server.js
@@ -3,5 +3,29 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/contactList', function(req, res) {
+    console.log('I received a GET request');
+    res.send('I received a GET request');
+    var contactList = [
+        {
+            name : "tim",
+            email : "time@me.com",
+            number : "(111) 111 1111"
+        },
+        {
+            name : "Emily",
+            email : "emily@me.com",
+            number : "(222) 222 2222"
+        },
+        {
+            name : "John",
+            email : "john@me.com",
+            number : "(333) 333 3333"
+        }
+
+    ];
+    res.json(contactList);
+});
+
 app.listen(3000);
 console.log('Listening on port 3000');
