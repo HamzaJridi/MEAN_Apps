75c3312007332dfde6032394813fa7767baa03b4
Hamza Jridi <jrid_hamza@yahoo.fr>
2016-04-23 14:47:19 +0100

    import users data from controller into index.html

--- public/controllers/AppCtrl.js
+++ public/controllers/AppCtrl.js
@@ -1,6 +1,23 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('AppCtrl', ['$scope', function($scope){
-    $scope.message = "Hello World from our Controller"
-    console.log('Hello World from our Controller');
+    $scope.message = "Hello World from our Controller";
+    $scope.contactList = [
+        {
+            name : "tim",
+            email : "time@me.com",
+            number : "(111) 111 1111"
+        },
+        {
+            name : "Emily",
+            email : "emily@me.com",
+            number : "(222) 222 2222"
+        },
+        {
+            name : "John",
+            email : "john@me.com",
+            number : "(333) 333 3333"
+        }
+
+    ];
 }]);
