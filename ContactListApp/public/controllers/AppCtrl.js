ea251811cfa69fb11a2db31f8883e814217703d8
Hamza Jridi <jrid_hamza@yahoo.fr>
2016-04-24 00:44:12 +0100

    send input data to the server by post req

--- ContactListApp/public/controllers/AppCtrl.js
+++ ContactListApp/public/controllers/AppCtrl.js
@@ -7,4 +7,11 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http){
         console.log('I got the requested data');
         $scope.contactlist = response;
     });
+    $scope.addContact = function(){
+        /**contact is the object data that we're sending to the server,
+         * it's bound by the ng-model from input fields in index.html*/
+        console.log($scope.contact);
+        //send the input data to the server
+        $http.post('/contactlist', $scope.contact);
+    }
 }]);
--- ContactListApp/server.js
+++ ContactListApp/server.js
@@ -3,9 +3,11 @@ var app = express();
 var mongojs = require('mongojs');
 //wich mongo db we're going to use
 var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
 
 //set the default folder for static files : html, js, css
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
 //set the route for the contactlist page
 app.get('/contactlist', function(req, res) {
@@ -14,6 +16,12 @@ app.get('/contactlist', function(req, res) {
         console.log(docs);
         res.json(docs);
     });
+});
+//listen to the post request from the Ctrl (input data by addContact method)
+app.post('/contactlist', function(req, res){
+    /**console the received data from the post req
+     * req.body require the 'body-parser module to teach the server how to parse the input's body'*/
+    console.log(req.body)
 });
 
 app.listen(3000);
