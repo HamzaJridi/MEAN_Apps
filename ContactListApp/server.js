456390eaf27e1413c1cd7dfb6fbaf12b5f59bb45
Hamza Jridi <jrid_hamza@yahoo.fr>
2016-04-23 23:38:53 +0100

    import data from the contactlist db into the index.html page

--- ContactListApp/server.js
+++ ContactListApp/server.js
@@ -10,26 +10,10 @@ app.use(express.static(__dirname + "/public"));
 //set the route for the contactlist page
 app.get('/contactlist', function(req, res) {
     console.log('I received a GET request');
-
-    person1 = {
-            name : "tim",
-            email:"time@me.com",
-            number:"(111) 111 1111"
-        };
-    person2 = {
-            name : "Emily",
-            email: "emily@me.com",
-            number: "(222) 222 2222"
-        };
-    person3 = {
-            name : "John",
-            email:"john@me.com",
-            number:"(333) 333 3333"
-        };
-
-    var contactlist = [person1, person2, person3];
-    //send the data in a json format
-    res.json(contactlist);
+    db.contactlist.find(function(err,docs){
+        console.log(docs);
+        res.json(docs);
+    });
 });
 
 app.listen(3000);
