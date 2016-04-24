865df7089d658b90751e6746a9c0e20af70b242a
Hamza Jridi <jrid_hamza@yahoo.fr>
2016-04-24 23:39:07 +0100

    set up routes using express.Router()

--- RESTful/app.js
+++ RESTful/app.js
@@ -2,10 +2,28 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
+var bookRouter = express.Router();
+/** set the book route using bookRouter.route('newRoute') method*/
+bookRouter.route('/Books')
+    .get(function(req,res){
+        var responseJson = {hello:'This is from the API'};
+        res.json(responseJson)
+    });
+
+
+/**
+ * bookRouter'll load all the routes of the app
+ *  every route'll be 'localhost:8000/api/newRoute'
+ *  if you set app.use('/', bookRouter) the routes will be 'localhost:8000/newRoute'
+ *  */
+app.use('/api', bookRouter);
+
+
+
 app.get('/', function(req, res){
     res.send('Welcome to my API')
 });
 
 app.listen(port, function(){
-    console.log('Gulp is running my app on port' + port);
+    console.log('Gulp is running the app on port' + port);
 });
