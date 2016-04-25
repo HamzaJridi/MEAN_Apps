2ccfe508d90fc503978b88d087d8b7a3751f1913
Hamza Jridi <jrid_hamza@yahoo.fr>
2016-04-25 17:53:45 +0100

    add a middleware to get the bookId and use it in get, put and patch reqs

--- RESTful/Routes/bookRoutes.js
+++ RESTful/Routes/bookRoutes.js
@@ -33,42 +33,65 @@ var routes = function(Book){
                 }
             });
         });
+    /** create a Middleware to get the bookId and use it
+     * in get,put and patch requests for a single book */
+    bookRouter.use('/:bookId',function(req,res,next){
+        /** getting a single book item by its Id */
+        //get the id passed in the url using req.params
+        Book.findById(req.params.bookId, function(err,book){
+           if(err){
+               res.status(500).send(err);
+           } else if (book){
+               req.book = book;
+           } else {
+               res.status(404).send('Book not found');
+           }
+        });
+    });
 
     bookRouter.route('/:bookId')
         //get a specific item by its id and display it
         .get(function(req,res){
-            /** getting a single book item by its Id */
-            //get the id passed in the url using req.params
-            Book.findById(req.params.bookId, function(err,book){
-                if(err){
-                    res.status(500).send(err);
-                } else {
-                    //display books data from the db as a json format
-                    res.json(book);
-                }
-            });
+            //req.book = book in the middleware
+            res.json(req.book);
         })
         //get a specific item by its id to update it
         .put(function(req,res){
             /** getting a single book item by its Id */
             //get the id passed in the url using req.params
-            Book.findById(req.params.bookId, function(err,book){
+            /**replace the book properties with what has
+             * come back from the req using req.body*/
+            req.book.title = req.body.title;
+            req.book.author = req.body.author;
+            req.book.genre = req.body.genre;
+            req.book.read = req.body.read;
+            //save changes in the db
+            req.book.save(function(err){
                 if(err){
                     res.status(500).send(err);
                 } else {
-                    /**replace the book properties with what has
-                     * come back from the req using req.body*/
-                    book.title = req.body.title;
-                    book.author = req.body.author;
-                    book.genre = req.body.genre;
-                    book.read = req.body.read;
-                    //save changes in the db
-                    book.save();
-                    //send back book to display it as a json frmt
-                    res.json(book);
+                    res.json(req.book);
+                }
+            });
+            //send back book to display it as a json frmt
+        })
+
+        .patch(function(req,res){
+            if (req.body._id) {
+                delete req.body._id;
+            }
+            for (var p in req.body) {
+                req.book[p] = req.body[p];
+            }
+            req.book.save(function(err){
+                if(err){
+                    res.status(500).send(err);
+                } else {
+                    res.json(req.book);
                 }
             });
         });
+
     return bookRouter
 };
 
