622500b22af4fcad8550753167ea5dd8ad84cb43
Hamza Jridi <jrid_hamza@yahoo.fr>
2016-04-26 15:36:58 +0100

    add posts the page and order them by 'upvotes'

--- MEAN_News/app.js
+++ MEAN_News/app.js
@@ -2,11 +2,20 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('MainCtrl', ['$scope', function($scope){
     $scope.test = 'Hello world from angular';
+    //list of posts ordered b up vote using angular filter
     $scope.posts = [
-        'post 1',
-        'post 2',
-        'post 3',
-        'post 4',
-        'post 5'
+        {title: 'post 1', upvotes: 5},
+        {title: 'post 2', upvotes: 2},
+        {title: 'post 3', upvotes: 15},
+        {title: 'post 4', upvotes: 9},
+        {title: 'post 5', upvotes: 4}
     ];
+    $scope.addPost = function(){
+        //prevent the user to add an empty post
+        if(!$scope.title || $scope.title === '') {
+            return;
+        }
+        $scope.posts.push({title: $scope.title, upvotes : 0});
+        $scope.addpost = "";
+    };
 }]);
