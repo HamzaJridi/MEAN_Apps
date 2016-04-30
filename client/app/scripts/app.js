d61a95bc277245932086920f04d1f85d661b05fe
Hamza Jridi <jrid_hamza@yahoo.fr>
2016-04-30 16:47:09 +0100

    bind tne angular app to tge rest api using the Movie factory

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -37,4 +37,24 @@ angular
               .otherwise({
                 redirectTo: '/'
         });
-  });
+  })
+    /** MovieRestangular Service :
+     * the Restangular prjct expects an 'id' arg but
+     * mongo db has an "_id", so this service will convert
+     * "_id" to "id"*/
+    .factory('MovieRestangular', function(Restangular) {
+        return Restangular.withConfig(function(RestangularConfigurer) {
+            RestangularConfigurer.setRestangularFields({
+                id: '_id'
+            });
+        });
+    })
+    /* The Movie factory is an object tha we can include in
+    * our controllers and it give us the ability to create
+    * and lidt new movies*/
+    .factory('Movie', function(MovieRestangular) {
+        return MovieRestangular.service('movie');
+        /*service('movie') : movie points to the url in the api
+        *--> at that API url I want to use the 'Movie' objct
+        * to pint to the 'movie' in point*/
+    });
