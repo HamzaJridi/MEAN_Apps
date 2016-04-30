210d42ae0a5a27547c6176e6aa04263c8a75e508
Hamza Jridi <jrid_hamza@yahoo.fr>
2016-04-30 01:04:08 +0100

    add the movie controller

---
+++ server/controllers/movieController.js
@@ -0,0 +1,17 @@
+var restful = ('node-restful');
+
+module.exports = function(app, route) {
+
+    // Setup the controller for REST;
+    var rest = restful.model(
+        'movie',app.models.movie
+    ).methods(['get', 'put', 'post', 'delete']);
+
+    //Register thr rest API to the app's route
+    rest.register(app, route);
+
+    //Return middleware
+    return function (req, res, next) {
+        next();
+    };
+};
