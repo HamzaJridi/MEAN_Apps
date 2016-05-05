55536aec4df35123c11c6145f5f0a828a514a20e
Hamza Jridi <jrid_hamza@yahoo.fr>
2016-05-05 20:19:32 +0100

    build a simple todoApp with Yeoman generator

---
+++ yeoman-todoApp/app/scripts/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yeomanApp
+ * @description
+ * # yeomanApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yeomanApp', [
+    'ngAnimate',
+    'ngRoute',
+    'ui.sortable',
+    'LocalStorageModule'
+  ])
+  .config(['localStorageServiceProvider', function(localStorageServiceProvider){
+    localStorageServiceProvider.setPrefix('ls');
+  }])
+
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/404', {
+        templateUrl: '404.html',
+        //controller: 'AboutCtrl',
+        //controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/404'
+      });
+  });
---
+++ yeoman-todoApp/app/scripts/controllers/main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yeomanApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yeomanApp
+ */
+angular.module('yeomanApp')
+  .controller('MainCtrl', function ($scope,localStorageService) {
+    //localStorage
+    var todosInStore = localStorageService.get('todos');
+
+    $scope.todos = todosInStore || [];
+
+    $scope.$watch('todos', function () {
+      localStorageService.set('todos', $scope.todos);
+    }, true);
+
+    $scope.addTodo = function () {
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+    };
+
+    $scope.removeTodo = function (index) {
+      $scope.todos.splice(index, 1);
+    };
+
+
+  });
